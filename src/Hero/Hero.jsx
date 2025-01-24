6326c2a9dea585f8c5268f4c96b31fef97c42fbb
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2025-01-24 19:28:25 +0100

    fixed animation

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,98 +1,133 @@
-import Header from '../Header/Header';
-import './Hero.css'
-import HeroPic from '../Assets/homepic.jpg'
-import {motion} from 'framer-motion'
-import { Fragment } from 'react';
+import Header from "../Header/Header";
+import "./Hero.css";
+import HeroPic from "../Assets/homepic.jpg";
+import { motion } from "framer-motion";
+import { Fragment } from "react";
 
+function Hero() {
+  /* CSS styled component*/
+  const styles = {
+    backgroundImage: `url(${HeroPic})`,
+    height: "90vh",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+  };
 
-function Hero () {
-    /* CSS styled component*/
-    const styles = {
-        backgroundImage: `url(${HeroPic})`,
-        height: '90vh',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        
-    }
+  /* Framer- motion component */
+  const containerVariants = {
+    hidden: {
+      opacity: 0,
+      x: 25, // Move the entire container down (from far up)
+    },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 3,
+        when: "beforeChildren", // Animate children before this container
+        staggerChildren: 0.3, // Stagger children animations
+      },
+    },
+  };
 
-    /* Framer- motion component */
-    const containerVariants = {
-        hidden: {
-          opacity: 0,
-          x: 25, // Move the entire container down (from far up)
-        },
-        show: {
-          opacity: 1,
-          x: 0,
-          transition: {
-            
-            duration: 3,
-            when: 'beforeChildren', // Animate children before this container
-            staggerChildren: 0.3, // Stagger children animations
-          },
-        },
-      };
-      
-      const childVariants = {
-        hidden: { y: 100, opacity: 0 },
-        show: { y: 0, opacity: 1,
-           transition: {
-             duration: 1.5,
-           }},
-      };
-      
-      const lastChildVariants = {
-        hidden: { x: -100, opacity: 0 },
-        show: { x: 0, opacity: 1,
-        transition: {
-          duration: 5
-        } },
-      };
-      
-      const buttonVariants = {
-        hidden: { y: 100, opacity: 0 },
-        show: { y: 0, opacity: 1,
-        transition: {
-          duration: 4
-        } },
-      }
-    
-      /* Hero------- component ------Function  */
-    return (
-      <Fragment >
+  const childVariants = {
+    hidden: { y: 100, opacity: 0 },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 1.5,
+      },
+    },
+  };
+
+  const lastChildVariants = {
+    hidden: { x: -100, opacity: 0 },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 5,
+      },
+    },
+  };
+
+  const buttonVariants = {
+    hidden: { y: 100, opacity: 0 },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 4,
+      },
+    },
+  };
+
+  /* Hero------- component ------Function  */
+  return (
+    <Fragment>
       <Header />
-        <motion.div style = {styles}
-            variants={containerVariants}
-            initial="hidden"
-            animate="show" 
-            id="Hero"
-        >
-         
-            <div className = 'hero--title'>
-                <motion.h2 className = 'hero-txt1'variants={childVariants}>SWEAT IN THE </motion.h2>
-                <motion.span className = 'hero--txt3'variants={childVariants}>IS THE GUARANTEE</motion.span>
-                <motion.span className = 'hero--txt4'variants={childVariants}> STAMP FOR YOUR</motion.span>
-                <motion.p className = 'hero--txt2'variants={childVariants}>GYM</motion.p>
-                
-                <motion.h3 className = 'hero--txt5'variants={lastChildVariants}> HEALTH</motion.h3>
-                <motion.div className='p' variants={childVariants}>
-                  <motion.p className='hero--txt--sent1' >Welcome to Gymnex Fitness Center, where strength meets dedication.</motion.p> 
-                  <motion.p className='hero--txt--sent2' >Unleash your full potential with our state-of-the-art facilities and expert trainers.</motion.p> 
-                  <motion.p className='hero--txt--sent3' >Join us today to sculpt a healthier, stronger you!</motion.p>
-                </motion.div>
-                
-                <motion.button className = 'hero--btn--get-started'variants={buttonVariants}
-                  whileHover={{scale:[1.1,1,1.1,1,1.1,1,1.1],  backgroundColor: '#2bbbad'}}
-                >Get Started</motion.button>
-               
-                <motion.button className = 'hero--btn--learn-more'variants={buttonVariants}
-                  whileHover={{scale:1.1, backgroundColor: '#8DB600' }}
-                >Learn More</motion.button>
-            </div>
-        </motion.div>
-        </Fragment>
-    )
-      
-}
-export default Hero
+      <motion.div
+        style={styles}
+        variants={containerVariants}
+        initial="hidden"
+        animate="show"
+        id="hero"
+      >
+        <div className="hero--title">
+          <motion.h2 className="hero-txt1" variants={childVariants}>
+            SWEAT IN THE{" "}
+          </motion.h2>
+          <motion.span className="hero--txt3" variants={childVariants}>
+            STAMP FOR YOUR
+          </motion.span>
+          <motion.span className="hero--txt4" variants={childVariants}>
+            {" "}
+            IS THE GUARANTEE
+          </motion.span>
+          <motion.p className="hero--txt2" variants={childVariants}>
+            GYM
+          </motion.p>
 
+          <motion.h3 className="hero--txt5" variants={lastChildVariants}>
+            {" "}
+            HEALTH
+          </motion.h3>
+          <motion.div className="p" variants={childVariants}>
+            <motion.p className="hero--txt--sent1">
+              Welcome to Gymnex Fitness Center, where strength meets dedication.
+            </motion.p>
+            <motion.p className="hero--txt--sent2">
+              Unleash your full potential with our state-of-the-art facilities
+              and expert trainers.
+            </motion.p>
+            <motion.p className="hero--txt--sent3">
+              Join us today to sculpt a healthier, stronger you!
+            </motion.p>
+          </motion.div>
+
+          <motion.button
+            className="hero--btn--get-started"
+            variants={buttonVariants}
+            whileHover={{
+              scale: [1.1, 1, 1.1, 1, 1.1, 1, 1.1],
+              backgroundColor: "#2bbbad",
+            }}
+          >
+            Get Started
+          </motion.button>
+
+          <motion.button
+            className="hero--btn--learn-more"
+            variants={buttonVariants}
+            whileHover={{ scale: 1.1, backgroundColor: "#8DB600" }}
+          >
+            Learn More
+          </motion.button>
+        </div>
+      </motion.div>
+    </Fragment>
+  );
+}
+export default Hero;
--- src/Reasons/Reasons.jsx
+++ src/Reasons/Reasons.jsx
@@ -1,50 +1,89 @@
-
-import Grids from '../Assets/Grids.png'
-import puma from '../Assets/puma-icon.png'
-import adidas from '../Assets/adidas-icon.png'
-import fila from '../Assets/fila-icon.png'
+import Grids from "../Assets/Grids.png";
+import puma from "../Assets/puma-icon.png";
+import adidas from "../Assets/adidas-icon.png";
+import fila from "../Assets/fila-icon.png";
 import { BsFillBookmarkCheckFill } from "react-icons/bs";
-import './Reasons.css';
-import {motion} from 'framer-motion'
-import { Fragment } from 'react';
+import "./Reasons.css";
+import { motion } from "framer-motion";
+import { Fragment } from "react";
 
-function Reasons ()  {
-  const transition = {type: "spring", duration: 3};
+function Reasons() {
+  const transition = { type: "spring", duration: 3 };
 
-    return (
-      <Fragment >
-        <div className='Reasons' id ='Reasons'>
-          <div className='left-r'> 
-            <img src={Grids} alt='Grids'/>
-          </div>
-        
-        <div className='right-r'>
-          <h2 className='right--head--1'>SOME REASONS</h2>
-          <h1 className='right--head--2'><span className='right--head--why'>WHY</span> CHOOSE US?</h1>
-          <div className='h5--reasons'>
+  return (
+    <Fragment>
+      <div className="Reasons" id="Reasons">
+        <div className="left-r">
+          <img src={Grids} alt="Grids" />
+        </div>
+
+        <div className="right-r">
+          <h2 className="right--head--1">SOME REASONS</h2>
+          <h1 className="right--head--2">
+            <span className="right--head--why">WHY</span> CHOOSE US?
+          </h1>
+          <div className="h5--reasons">
             <ul>
-              <motion.li whileHover={{x: '10px'}}><span className='reason--mark'><BsFillBookmarkCheckFill /></span>OVER 40+ EXPERT TRAINERS</motion.li>
-              <motion.li whileHover={{x: '10px'}}><span className='reason--mark'><BsFillBookmarkCheckFill /></span>MODERN AND INVITING AMBIANCE</motion.li>
-              <motion.li whileHover={{x: '10px'}}><span className='reason--mark'><BsFillBookmarkCheckFill /></span>REGULAR FITNESS CHALLENGES</motion.li>
-              <motion.li whileHover={{x: '10px'}}><span className='reason--mark'><BsFillBookmarkCheckFill /></span>HEALTH BENEFITS</motion.li>
-              <motion.li whileHover={{x: '10px'}}><span className='reason--mark'><BsFillBookmarkCheckFill /></span>24/7 ACCESS</motion.li>
-              <motion.li whileHover={{x: '10px'}}><span className='reason--mark'><BsFillBookmarkCheckFill /></span>RELIABLE PARTNERS</motion.li>
+              <motion.li whileHover={{ x: "10px" }}>
+                <span className="reason--mark">
+                  <BsFillBookmarkCheckFill />
+                </span>
+                OVER 40+ EXPERT TRAINERS
+              </motion.li>
+              <motion.li whileHover={{ x: "10px" }}>
+                <span className="reason--mark">
+                  <BsFillBookmarkCheckFill />
+                </span>
+                MODERN AND INVITING AMBIANCE
+              </motion.li>
+              <motion.li whileHover={{ x: "10px" }}>
+                <span className="reason--mark">
+                  <BsFillBookmarkCheckFill />
+                </span>
+                REGULAR FITNESS CHALLENGES
+              </motion.li>
+              <motion.li whileHover={{ x: "10px" }}>
+                <span className="reason--mark">
+                  <BsFillBookmarkCheckFill />
+                </span>
+                HEALTH BENEFITS
+              </motion.li>
+              <motion.li whileHover={{ x: "10px" }}>
+                <span className="reason--mark">
+                  <BsFillBookmarkCheckFill />
+                </span>
+                24/7 ACCESS
+              </motion.li>
+              <motion.li whileHover={{ x: "10px" }}>
+                <span className="reason--mark">
+                  <BsFillBookmarkCheckFill />
+                </span>
+                RELIABLE PARTNERS
+              </motion.li>
             </ul>
           </div>
-          <h3 className='partner--head'>OUR PARTNERS</h3>
-          <motion.div
-          initial={{opacity: 0, x: 100}}
-          transition={{...transition, duration: 4}}
-          whileInView={{opacity: 1, x: 0}}
-          className='partner--icon'
-          >
-            <span><img className='puma' src={puma} alt='Puma'/></span>
-            <span><img className='adidas' src={adidas} alt='Adidas'/></span>
-            <span><img className='fila' src={fila} alt='Fila'/></span>
-          </motion.div>
+          <div>
+            <h3 className="partner--head">OUR PARTNERS</h3>
+            <motion.div
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ ...transition, duration: 4 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              className="partner--icon"
+            >
+              <span>
+                <img className="puma" src={puma} alt="Puma" />
+              </span>
+              <span>
+                <img className="adidas" src={adidas} alt="Adidas" />
+              </span>
+              <span>
+                <img className="fila" src={fila} alt="Fila" />
+              </span>
+            </motion.div>
+          </div>
         </div>
       </div>
-      </Fragment>
-    );
-  }
-export default Reasons
+    </Fragment>
+  );
+}
+export default Reasons;
